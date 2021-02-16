f80e5035a7ac9b89ec6fb8eca7752541a2f39b1c
Sascha <36052404+hellovertex@users.noreply.github.com>
2021-02-16 16:31:10 +0100

    Conservative agent (#5)
    * changed to test hle functions
    
    * reward shaping class conservative agent implemented
    
    * reward shaping class
    
    * enable creation of vectorized stacker in rainbow agent, add placeholder functions to rulebased agent
    
    * adapt observation size of buffer to enable stacking
    
    * bug fix return value of eval_policy
    
    * deactivate discard shaping
    
    * return reward shaping types
    
    * risk shaping lin dep on game performance
    
    * add reward shaping for conservative play moves
    
    * debug reward shaping for conservative play moves
    
    * enable schedule for reward shaping parameters
    
    * delete
    
    * Update reward_shaping.py
    
    remove timeit functions
    
    * query if discard shaping activated
    
    * add string representation of reward shaper
    
    * store and load optimizer state
    
    * remove unneeded package
    
    * store state of experiments buffer for checkpoint
    
    Co-authored-by: Vanessa Soisson <v.soisson@web.de>
    Co-authored-by: vsois <69348432+vsois@users.noreply.github.com>

--- hanabi_agents/rlax_dqn/params.py
+++ hanabi_agents/rlax_dqn/params.py
@@ -27,7 +27,11 @@ class RewardShapingParams(NamedTuple):
     # conservative agent
     shaper: bool = True
     min_play_probability: float = 0.8
-    w_play_probability: float = 0
+    w_play_penalty: Union[Callable[[int], float], float] = 0
+    m_play_penalty: float = 0
+    w_play_reward: Union[Callable[[int], float], float] = 0
+    m_play_reward: float = 0
+    
     penalty_last_of_kind: float = 0
 
 @gin.configurable
--- hanabi_agents/rlax_dqn/reward_shaping.py
+++ hanabi_agents/rlax_dqn/reward_shaping.py
@@ -2,11 +2,13 @@
 from .params import RewardShapingParams
 import numpy as np
 from hanabi_learning_environment import pyhanabi_pybind as pyhanabi
+from collections import Counter
 
 class ShapingType:
     NONE=0
     RISKY=1
     DISCARD_LAST_OF_KIND=2
+    CONSERVATIVE=3
 
 # reward shaping class
 class RewardShaper:
@@ -14,39 +16,65 @@ class RewardShaper:
     def __init__(self,
                  params: RewardShapingParams = RewardShapingParams()):
         
+        if not callable(params.w_play_penalty):
+            penalty = params.w_play_penalty
+            params = params._replace(w_play_penalty=lambda ts: penalty)
+        if not callable(params.w_play_reward):
+            reward = params.w_play_reward
+            params = params._replace(w_play_reward=lambda ts: reward)
+        
         self.params = params
-        self.num_ranks = None
         self.unshaped = (0, ShapingType.NONE)
+        
+        # auxiliary variables
+        self.num_ranks = None
+        self._performance = 0
+        self._m_play_penalty = 0
+        self._m_play_reward = 0
+    
+    @property
+    def performance(self):
+        return self._performance
+    
+    @performance.setter
+    def performance(self, performance):
+        self._performance = performance
+        self._m_play_penalty = self.params.m_play_penalty * self._performance
+        self._m_play_reward = self.params.m_play_reward * self._performance
 
-    def shape(self, observations, moves):
+    def shape(self, observations, moves, step):
         
         assert len(observations) == len(moves)
 
         if self.num_ranks == None:
             for obs in observations:
                 self.num_ranks = obs.parent_game.num_ranks
-
-        shaped_rewards = [self._calculate(obs, move)for obs, move in zip(observations, moves)]
+                
+        shaped_rewards = [self._calculate(obs, move, step)
+                          for obs, move in zip(observations, moves)]
         return zip(*shaped_rewards)
                     
-    def _calculate(self, observation, move):
+    def _calculate(self, observation, move, step):
                 
         if move.move_type == pyhanabi.HanabiMove.Type.kPlay:
-            return self._play_shape(observation, move)
+            return self._play_shape(observation, move, step)
         if move.move_type == pyhanabi.HanabiMove.Type.kDiscard:
-            return self._discard_shape(observation, move)
+            return self._discard_shape(observation, move, step)
         if move.move_type in [pyhanabi.HanabiMove.Type.kRevealColor,
                               pyhanabi.HanabiMove.Type.kRevealRank]:
-            return self._hint_shape(observation, move)
+            return self._hint_shape(observation, move, step)
         else:
             return self.unshaped
             
-    def _discard_shape(self, observation, move):
+    def _discard_shape(self, observation, move, step):
+        
+        if self.params.penalty_last_of_kind==0:
+            return self.unshaped
 
         discard_pile = observation.discard_pile
         card_index = move.card_index
         discarded_card = observation.card_to_discard(card_index)
-         
+
         if discarded_card.rank == self.num_ranks -1:
             return (self.params.penalty_last_of_kind, ShapingType.DISCARD_LAST_OF_KIND)
         
@@ -68,19 +96,25 @@ class RewardShaper:
                 return (self.params.penalty_last_of_kind, ShapingType.DISCARD_LAST_OF_KIND)
             else:
                 return self.unshaped
-    
-    def _hint_shape(self, observation, move):
+
+    def _hint_shape(self, observation, move, step):
         return self.unshaped
     
-    def _play_shape(self, observation, move):
+    def _play_shape(self, observation, move, step):
         
-        # the move may be illegal, eg. playing a card that is not available in hand
         try:
             prob = observation.playable_percent()[move.card_index]
         except IndexError:
             return self.unshaped
         
         if prob < self.params.min_play_probability:
-            return (self.params.w_play_probability, ShapingType.RISKY)
 
-        return self.unshaped
+            penalty = self.params.w_play_penalty(step) + self._m_play_penalty
+            return (penalty, ShapingType.RISKY)
+
+        reward = self.params.w_play_reward(step) + self._m_play_reward
+        return (reward, ShapingType.CONSERVATIVE)
+    
+    def __repr__(self):
+        return f"<rlax_dqn.RewardShaper(params={self.params})>"
+
