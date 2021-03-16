c8de17a65e206a7faf63a0d839a2ce5d7632487a
msagemue <m.sagemueller@campus.tu-berlin.de>
2021-03-16 15:37:43 +0100

    init file adapted

--- hanabi_agents/rlax_dqn/__init__.py
+++ hanabi_agents/rlax_dqn/__init__.py
@@ -1,5 +1,6 @@
 #  from .rlax_dqn import DQNAgent
 from .rlax_rainbow import DQNAgent
+from .rlax_rainbow_parallel import DQNAgent as DQNParallel
 from .params import RlaxRainbowParams, RewardShapingParams, PBTParams
 from .reward_shaping import RewardShaper, ShapingType
 from .vectorized_stacker import VectorizedObservationStacker
